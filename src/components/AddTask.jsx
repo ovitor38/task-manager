e22c87d416e4564f292afc1c31a4234f2c3e3796
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-04-02 21:15:04 -0300

    feat: fetch task after addtion

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -8,26 +8,35 @@ import "./AddTask.scss";
 import { useAlert } from "react-alert";
 import axios from "axios";
 
-const AddTask = () => {
+const AddTask = ({ fetchTasks }) => {
     const [task, setTask] = useState("");
 
-    alert = useAlert()
+    alert = useAlert();
 
     const onChange = (e) => {
         setTask(e.target.value);
     };
 
     const handleTaskAddition = async () => {
-      try {
-        if(task.length ===0){
-          return alert.error('A tarefa precisa de uma descrição para ser adicionada')
-        }
+        try {
+            if (task.length === 0) {
+                return alert.error(
+                    "A tarefa precisa de uma descrição para ser adicionada"
+                );
+            }
 
-        await axios.post('http://localhost:8000/tasks', {description: task, isCompleted: false})
-      } catch (error) {
-        
-      }
-    } 
+            await axios.post("http://localhost:8000/tasks", {
+                description: task,
+                isCompleted: false,
+            });
+
+            await fetchTasks();
+
+            setTask("");
+        } catch (error) {
+            alert.erro("Algo deu errado");
+        }
+    };
 
     return (
         <div className="add-task-container">
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -27,7 +27,7 @@ const Tasks = () => {
 
             <div className="last-tasks">
                 <h3>Últimas Tarefas</h3>
-                <AddTask />
+                <AddTask fetchTasks={fetchTasks} />
                 <div className="tasks-list">
                     {tasks
                         .filter((task) => task.isCompleted === false)
