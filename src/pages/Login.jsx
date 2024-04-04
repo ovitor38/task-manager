5a5a9ac0d95334600f84216a757355c2812a9041
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-04-04 12:20:29 -0300

    feat: add Login componet

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,1 +1,16 @@
-import './Login.scss'
+import CustomButton from "../components/CustomButton";
+
+import "./Login.scss";
+
+import logo from "../assets/images/logo-2.png";
+
+const Login = () => {
+    return (
+        <div className="login-container">
+            <img src={logo} alt="Full Stack Club" />
+            <CustomButton>Entrar</CustomButton>
+        </div>
+    );
+};
+
+export default Login;
