f92d1b5a9b7af288512b5cd16eabb987d6127022
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-04-04 12:08:22 -0300

    feat: add react-router -dom and setup

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
 
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const options = {
     position: positions.BOTTOM_CENTER,
@@ -18,10 +19,12 @@ const options = {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <AlertProvider template={AlertTemplate} {...options}>
-            <App />
-        </AlertProvider>
-    </React.StrictMode>,
+        <BrowserRouter>
+            <AlertProvider template={AlertTemplate} {...options}>
+                <App />
+            </AlertProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 reportWebVitals();
